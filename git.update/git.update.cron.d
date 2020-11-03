e14c4c08786f368c1ab802cfa341bdec3a72ea1f
BoarderEB <info@enrico-kunze>
2020-11-03 12:35:35 +0100

    change location git.update.sh to /usr/bin/

--- git.update/git.update.cron.d
+++ git.update/git.update.cron.d
@@ -1,4 +1,4 @@
 ## /etc/cron.d/ EXAMPLE
 # TODO SET PATH TO git.update.sh
 # EVERY 5 MINUTES START UPDATE OF REPOSITORYS
-*/5 * * * * root /path/git.update.sh >/dev/null 2>&1
+*/5 * * * * root /usr/bin/git.update.sh >/dev/null 2>&1
