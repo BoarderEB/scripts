bbe263f97ac4152553e68509b0ee86627b1f11ae
BoarderEB <info@enrico-kunze>
2020-11-03 12:38:00 +0100

    test that git.list exists

--- git.update/git.update.sh
+++ git.update/git.update.sh
@@ -3,6 +3,12 @@
 ##STRUCTURE git.LIST
 ##/path/to/git/root/:branch
 file="/etc/git/git.list"
+
+if [ ! -f "$file" ]; then
+    echo "$file don't exists. Exit"
+    exit 1
+else 
+
 while IFS=: read -r f1 f2
 do
   ##CHECK IF PATH OF LINE IS NOT EMPTY
