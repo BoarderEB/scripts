2bfb6d79ddf6c514045807ed5b4fe5c92213fde9
BoarderEB <info@enrico-kunze>
2020-11-03 12:05:05 +0100

    some test of git.list

--- git.update/git.update.sh
+++ git.update/git.update.sh
@@ -2,11 +2,22 @@
 ##READ FROM GIT-LIST FILE AND PULL ALL
 ##STRUCTURE git.LIST
 ##/path/to/git/root/:branch
-file="/root/scripts/git.list"
+file="git.list"
 while IFS=: read -r f1 f2
 do
-        git -C $f1 fetch --all
-        git -C $f1 checkout $f2
-        git -C $f1 reset --hard $f2
-        git -C $f1 pull
-done <"$file
+  ##CHECK IF PATH OF LINE IS NOT EMPTY
+  if [[  ! -z "$f1"  ]]; then
+    ##CHECK IF BRANCH OF LINE IS NOT EMPTY
+    if [[  ! -z "$f2"  ]]; then
+      ##CHECK IF PATH OF LINE EXISTS
+      if [[ -d "$f1" ]]; then
+        git -C "$f1" fetch --all && git -C "$f1" checkout "$f2" && git -C "$f1" reset --hard "$f2" && git -C "$f1" pull
+      else
+        ##CHECK IF LINE IS NOT COMMENDET OUT
+        if [[ ! "$f1" =~ ^\s*# ]]; then
+        echo "directory $f1 directory does not exist"
+        fi
+      fi
+    fi
+  fi
+done <"$file"
